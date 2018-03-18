22a8859e3189d9a932a0aaa685e2b33e2c9992d1
Madeline Stark <madeline.stark1@gmail.com>
2018-03-18 22:26:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    new_array = []
+    array.each do|customer|
+    new_array << " #{customer.with_index}"
+    end
+    puts "The line is currently: #{new_array.join}"
+  end
+end
+
+#.withindex-call after .each method...provides index variable-can use that on line 8-try interpolating in
+
+#def take_a_number(array, customer)
+  #number = line(array, customer) + 1
+  #return number + customer
+#end
